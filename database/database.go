dd3df6780f84728131bf1999ccc58a248232ab9a
Grant Barnes <gfbarnes78@gmail.com>
2024-07-31 20:04:17 -0500

    adding join lobby page

--- database/database.go
+++ database/database.go
@@ -30,6 +30,47 @@ func Ping(dbcs string) error {
 	return nil
 }
 
+func GetLobbies(dbcs string) ([]Lobby, error) {
+	db, err := sql.Open("mysql", dbcs)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	selectStatment, err := db.Prepare(`
+		SELECT ID
+			 , DATE_ADDED
+			 , DATE_MODIFIED
+			 , NAME
+			 , PASSWORD
+	 	FROM LOBBY 
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer selectStatment.Close()
+
+	rows, err := selectStatment.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]Lobby, 0)
+	for rows.Next() {
+		var lobby Lobby
+		if err := rows.Scan(
+			&lobby.Id,
+			&lobby.DateAdded,
+			&lobby.DateModified,
+			&lobby.Name,
+			&lobby.Password); err != nil {
+			continue
+		}
+		result = append(result, lobby)
+	}
+	return result, nil
+}
+
 func GetJudgeCards(dbcs string) ([]Card, error) {
 	db, err := sql.Open("mysql", dbcs)
 	if err != nil {
@@ -67,8 +108,7 @@ func GetJudgeCards(dbcs string) ([]Card, error) {
 			&card.DeckId,
 			&card.Type,
 			&card.Text); err != nil {
-			fmt.Println(err)
-			break
+			continue
 		}
 		result = append(result, card)
 	}
