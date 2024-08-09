48eda417cfec84586a0eb7b06b10d48b41f91803
Grant Barnes <gfbarnes78@gmail.com>
2024-08-09 15:01:21 -0500

    creating deck list page, adding links to home page

--- database/database.go
+++ database/database.go
@@ -71,6 +71,47 @@ func GetLobbies(dbcs string) ([]Lobby, error) {
 	return result, nil
 }
 
+func GetDecks(dbcs string) ([]Deck, error) {
+	db, err := sql.Open("mysql", dbcs)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	selectStatment, err := db.Prepare(`
+		SELECT ID
+			 , DATE_ADDED
+			 , DATE_MODIFIED
+			 , NAME
+			 , PASSWORD
+	 	FROM DECK 
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer selectStatment.Close()
+
+	rows, err := selectStatment.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]Deck, 0)
+	for rows.Next() {
+		var deck Deck
+		if err := rows.Scan(
+			&deck.Id,
+			&deck.DateAdded,
+			&deck.DateModified,
+			&deck.Name,
+			&deck.Password); err != nil {
+			continue
+		}
+		result = append(result, deck)
+	}
+	return result, nil
+}
+
 func GetJudgeCards(dbcs string) ([]Card, error) {
 	db, err := sql.Open("mysql", dbcs)
 	if err != nil {
