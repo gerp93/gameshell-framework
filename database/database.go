905cd19751da7f8c9a8c9acc3f3bcd1e1c68c3e3
Grant Barnes <gfbarnes78@gmail.com>
2024-08-09 15:34:59 -0500

    adding deck to cards page

--- database/database.go
+++ database/database.go
@@ -113,6 +113,48 @@ func GetDecks(dbcs string) ([]Deck, error) {
 	return result, nil
 }
 
+func GetDeck(dbcs string, id uuid.UUID) (Deck, error) {
+	var deck Deck
+
+	db, err := sql.Open("mysql", dbcs)
+	if err != nil {
+		return deck, err
+	}
+	defer db.Close()
+
+	selectStatment, err := db.Prepare(`
+		SELECT ID
+			 , DATE_ADDED
+			 , DATE_MODIFIED
+			 , NAME
+			 , PASSWORD
+	 	FROM DECK 
+		WHERE ID = ?
+	`)
+	if err != nil {
+		return deck, err
+	}
+	defer selectStatment.Close()
+
+	rows, err := selectStatment.Query(id)
+	if err != nil {
+		return deck, err
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(
+			&deck.Id,
+			&deck.DateAdded,
+			&deck.DateModified,
+			&deck.Name,
+			&deck.Password); err != nil {
+			return deck, err
+		}
+	}
+
+	return deck, nil
+}
+
 func GetCards(dbcs string, deckId uuid.UUID) ([]Card, error) {
 	db, err := sql.Open("mysql", dbcs)
 	if err != nil {
