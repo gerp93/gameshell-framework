f7bdee7c83975f2670fbb72824b62c98507e7cda
Grant Barnes <gfbarnes78@gmail.com>
2024-08-25 17:56:25 -0500

    adding lobby create api

--- database/database.go
+++ database/database.go
@@ -114,6 +114,39 @@ func GetLobby(dbcs string, id uuid.UUID) (Lobby, error) {
 	return lobby, nil
 }
 
+func CreateLobby(dbcs string, name string, password string) (uuid.UUID, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return id, err
+	}
+
+	db, err := sql.Open("mysql", dbcs)
+	if err != nil {
+		return id, err
+	}
+	defer db.Close()
+
+	insertStatment, err := db.Prepare(`
+		INSERT INTO LOBBY (ID, NAME, PASSWORD)
+		VALUES (?, ?, ?)
+	`)
+	if err != nil {
+		return id, err
+	}
+	defer insertStatment.Close()
+
+	if password == "" {
+		_, err = insertStatment.Exec(id, name, nil)
+	} else {
+		_, err = insertStatment.Exec(id, name, password)
+	}
+	if err != nil {
+		return id, err
+	}
+
+	return id, nil
+}
+
 func GetDecks(dbcs string) ([]Deck, error) {
 	db, err := sql.Open("mysql", dbcs)
 	if err != nil {
