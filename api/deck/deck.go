9b04a7e0d1dbb946c92351ed19d81f8c251f6c1b
Grant Barnes <gfbarnes78@gmail.com>
2024-08-30 11:38:52 -0500

    creating access specific api calls

--- api/deck/deck.go
+++ api/deck/deck.go
@@ -9,51 +9,6 @@ import (
 	"github.com/grantfbarnes/card-judge/database"
 )
 
-func Access(w http.ResponseWriter, r *http.Request) {
-	idString := r.PathValue("id")
-	id, err := uuid.Parse(idString)
-	if err != nil {
-		api.WriteBadHeader(w, http.StatusBadRequest, "Failed to get id from path.")
-		return
-	}
-
-	dbcs := database.GetDatabaseConnectionString()
-	deck, err := database.GetDeck(dbcs, id)
-	if err != nil {
-		api.WriteBadHeader(w, http.StatusBadRequest, "Failed to update the database.")
-		return
-	}
-
-	err = r.ParseForm()
-	if err != nil {
-		api.WriteBadHeader(w, http.StatusBadRequest, "Failed to parse form.")
-		return
-	}
-
-	var password string
-	for key, val := range r.Form {
-		if key != "password" {
-			continue
-		}
-		password = val[0]
-		break
-	}
-
-	if !auth.PasswordMatchesHash(password, deck.PasswordHash.String) {
-		api.WriteBadHeader(w, http.StatusBadRequest, "Provided password is not valid.")
-		return
-	}
-
-	err = auth.AddCookieAccessId(w, r, deck.Id)
-	if err != nil {
-		api.WriteBadHeader(w, http.StatusBadRequest, "Failed to set cookie in browser.")
-		return
-	}
-
-	w.Header().Add("HX-Refresh", "true")
-	api.WriteGoodHeader(w, http.StatusCreated, "Success")
-}
-
 func Create(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
