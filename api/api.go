8cfc2850b2f89a1a0f560a03ec210884b281fd9d
Grant Barnes <gfbarnes78@gmail.com>
2024-08-30 08:48:05 -0500

    update middleware to gather base page data

--- api/api.go
+++ api/api.go
@@ -2,38 +2,7 @@ package api
 
 import (
 	"net/http"
-	"strings"
-
-	"github.com/grantfbarnes/card-judge/auth"
 )
-
-func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := auth.GetCookiePlayerId(r)
-		loggedIn := err == nil
-
-		// required to be logged in
-		if r.URL.Path == "/manage" ||
-			strings.HasPrefix(r.URL.Path, "/lobby/") ||
-			strings.HasPrefix(r.URL.Path, "/deck/") {
-			if !loggedIn {
-				auth.SetCookieRedirectURL(w, r.URL.Path)
-				http.Redirect(w, r, "/login", http.StatusSeeOther)
-				return
-			}
-		}
-
-		// required to not be logged in
-		if r.URL.Path == "/login" {
-			if loggedIn {
-				http.Redirect(w, r, auth.GetCookieRedirectURL(r), http.StatusSeeOther)
-				return
-			}
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func WriteGoodHeader(w http.ResponseWriter, statusCode int, message string) {
 	w.Header().Add("HX-Retarget", "find .htmx-result-good")
