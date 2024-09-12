7810f405943dbda32656d940e91f5041bf47e589
Grant Barnes <gfbarnes78@gmail.com>
2024-09-12 18:14:44 -0500

    create hand draw stored procedure, auto draw when player joins lobby

--- database/player.go
+++ database/player.go
@@ -124,45 +124,12 @@ func GetPlayerGameBoard(playerId uuid.UUID) (data playerGameBoard, err error) {
 }
 
 func DrawPlayerHand(playerId uuid.UUID) (data playerData, err error) {
-	handCount, err := getPlayerHandCount(playerId)
+	sqlString := `
+		CALL SP_DRAW_HAND (?)
+	`
+	err = Execute(sqlString, playerId)
 	if err != nil {
 		return data, err
-	}
-
-	handSize, err := getPlayerHandSize(playerId)
-	if err != nil {
-		return data, err
-	}
-
-	cardsToDraw := handSize - handCount
-	if cardsToDraw > 0 {
-		sqlString := `
-			INSERT INTO HAND
-				(
-					PLAYER_ID,
-					CARD_ID
-				)
-			SELECT DISTINCT
-				P.ID AS PLAYER_ID,
-				C.ID AS CARD_ID
-			FROM DRAW_PILE AS DP
-				INNER JOIN PLAYER AS P ON P.LOBBY_ID = DP.LOBBY_ID
-				INNER JOIN CARD AS C ON C.ID = DP.CARD_ID
-				INNER JOIN CARD_TYPE AS CT ON CT.ID = C.CARD_TYPE_ID
-			WHERE CT.NAME = 'Player'
-				AND P.ID = ?
-			ORDER BY RAND()
-			LIMIT ?
-		`
-		err = Execute(sqlString, playerId, cardsToDraw)
-		if err != nil {
-			return data, err
-		}
-
-		err = removeUserHandFromLobbyCards()
-		if err != nil {
-			return data, err
-		}
 	}
 
 	return GetPlayerData(playerId)
@@ -277,37 +244,6 @@ func getPlayerHandSize(playerId uuid.UUID) (handSize int, err error) {
 	}
 
 	return handSize, nil
-}
-
-func getPlayerHandCount(playerId uuid.UUID) (handCount int, err error) {
-	sqlString := `
-		SELECT
-			COUNT(CARD_ID)
-		FROM HAND
-		WHERE PLAYER_ID = ?
-	`
-	rows, err := Query(sqlString, playerId)
-	if err != nil {
-		return handCount, err
-	}
-
-	for rows.Next() {
-		if err := rows.Scan(&handCount); err != nil {
-			return handCount, err
-		}
-	}
-
-	return handCount, nil
-}
-
-func removeUserHandFromLobbyCards() error {
-	sqlString := `
-		DELETE DP
-		FROM DRAW_PILE AS DP
-			INNER JOIN PLAYER AS P ON P.LOBBY_ID = DP.LOBBY_ID
-			INNER JOIN HAND AS H ON H.PLAYER_ID = P.ID AND H.CARD_ID = DP.CARD_ID
-	`
-	return Execute(sqlString)
 }
 
 func getPlayerHand(playerId uuid.UUID) ([]Card, error) {
