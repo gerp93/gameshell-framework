4d1e49b4da51656f12009ae20cc4409db5d3a747
Grant Barnes <gfbarnes78@gmail.com>
2026-05-24 10:19:45 -0500

    do not put wild deck deletes into audit deck table

--- static/sql/triggers/TR_AUDIT_DECK_DELETE.sql
+++ static/sql/triggers/TR_AUDIT_DECK_DELETE.sql
@@ -3,40 +3,50 @@ OR REPLACE TRIGGER TR_AUDIT_DECK_DELETE
 BEFORE DELETE ON DECK
 FOR EACH ROW
 BEGIN
-    INSERT INTO AUDIT_CARD(
-        AUDIT_TYPE,
-        CARD_ID,
-        DECK_ID,
-        CATEGORY,
-        TEXT,
-        YOUTUBE,
-        IMAGE
-    )
-    SELECT
-        'DELETE',
-        ID,
-        DECK_ID,
-        CATEGORY,
-        TEXT,
-        YOUTUBE,
-        IMAGE
-    FROM CARD
-    WHERE DECK_ID = OLD.ID;
+    DECLARE VAR_IS_WILD_DECK BOOLEAN DEFAULT (
+            SELECT
+                IS_LOBBY_WILD_DECK
+            FROM DECK
+            WHERE ID = OLD.ID
+        );
 
-    INSERT INTO AUDIT_DECK(
-        AUDIT_TYPE,
-        DECK_ID,
-        NAME,
-        PASSWORD_HASH,
-        IS_PUBLIC_READONLY,
-        IS_LOBBY_WILD_DECK
-    )
-    VALUES (
-        'DELETE',
-        OLD.ID,
-        OLD.NAME,
-        OLD.PASSWORD_HASH,
-        OLD.IS_PUBLIC_READONLY,
-        OLD.IS_LOBBY_WILD_DECK
-    );
+    IF NOT VAR_IS_WILD_DECK THEN
+        INSERT INTO AUDIT_CARD(
+            AUDIT_TYPE,
+            CARD_ID,
+            DECK_ID,
+            CATEGORY,
+            TEXT,
+            YOUTUBE,
+            IMAGE
+        )
+        SELECT
+            'DELETE',
+            ID,
+            DECK_ID,
+            CATEGORY,
+            TEXT,
+            YOUTUBE,
+            IMAGE
+        FROM CARD
+        WHERE DECK_ID = OLD.ID;
+
+        INSERT INTO AUDIT_DECK(
+            AUDIT_TYPE,
+            DECK_ID,
+            NAME,
+            PASSWORD_HASH,
+            IS_PUBLIC_READONLY,
+            IS_LOBBY_WILD_DECK
+        )
+        VALUES (
+            'DELETE',
+            OLD.ID,
+            OLD.NAME,
+            OLD.PASSWORD_HASH,
+            OLD.IS_PUBLIC_READONLY,
+            OLD.IS_LOBBY_WILD_DECK
+        );
+    END
+    IF;
 END;
